7b121a510eb49c47fd88ae2e9bf835117f875901
Valentyn Bakhmatskyi <val.bakhmatskyi@gmail.com>
2025-11-14 17:50:18 +0200

    feat(backend): configure sequelize and database connection

--- backend/src/configurations/index.ts
+++ backend/src/configurations/index.ts
@@ -1,3 +1,7 @@
 export default () => ({
   port: process.env.PORT,
+  db_port: process.env.DB_PORT,
+  db_user: process.env.DB_USER,
+  db_password: process.env.DB_PASSWORD,
+  db_name: process.env.DB_NAME,
 });
