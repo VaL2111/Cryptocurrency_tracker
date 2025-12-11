fc98d4d09fb9ea7165b5854c1802beae7a44c274
Valentyn Bakhmatskyi <val.bakhmatskyi@gmail.com>
2025-12-11 13:56:00 +0200

    feat(client): use dynamic API URL from env variables

--- frontend/src/utils/axios/index.ts
+++ frontend/src/utils/axios/index.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
 export const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: apiUrl,
   timeout: 1000,
   headers: { "X-Custom-Header": "foobar" },
 });
