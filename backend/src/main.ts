a6c98e73d1a329a40a61aa50b316f7b9ca14d4a8
Valentyn Bakhmatskyi <val.bakhmatskyi@gmail.com>
2025-12-11 22:35:54 +0200

    feat(backend): integrate 'Swagger' for API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./modules/app/app.module";
 import { ConfigService } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port: number = configService.get("port", 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle("API")
+    .setDescription("This api for labs")
+    .setVersion("1.0")
+    .addTag("API")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(port);
 }
 bootstrap();
