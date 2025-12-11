993fef142a901c07dbb8146981a25f20bdfca3fe
Valentyn Bakhmatskyi <val.bakhmatskyi@gmail.com>
2025-12-11 17:18:00 +0200

    fix(frontend): increase axios timeout for render

--- frontend/src/utils/axios/index.ts
+++ frontend/src/utils/axios/index.ts
@@ -4,6 +4,6 @@ const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export const instance = axios.create({
   baseURL: apiUrl,
-  timeout: 1000,
+  timeout: 20000,
   headers: { "X-Custom-Header": "foobar" },
 });
