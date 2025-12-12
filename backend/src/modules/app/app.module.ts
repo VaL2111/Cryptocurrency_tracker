006fb66965df2db14a7dc888d4e779e1f27ec03c
Valentyn Bakhmatskyi <val.bakhmatskyi@gmail.com>
2025-12-12 18:36:54 +0200

    feat(backend): implement create user endpoint with password hashing

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from "../user/user.module";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import configurations from "../../configurations";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { User } from "../user/models/user.model";
 
 @Module({
   imports: [
@@ -23,7 +24,7 @@ import { SequelizeModule } from "@nestjs/sequelize";
         database: configService.get("db_name"),
         synchronize: true,
         autoLoadModels: true,
-        models: [],
+        models: [User],
       }),
     }),
     UserModule,
--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from "@nestjs/common";
-import { users } from "../../moks";
+import * as bcrypt from "bcrypt";
+import { InjectModel } from "@nestjs/sequelize";
+import { User } from "./models/user.model";
+import { CreateUserDTO } from "./dto";
 
 @Injectable()
 export class UserService {
-  getUsers() {
-    return users;
+  constructor(
+    @InjectModel(User) private readonly userRepository: typeof User,
+  ) {}
+
+  async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
+  }
+
+  async createUser(dto: CreateUserDTO) {
+    const hashedPassword = await this.hashPassword(dto.password);
+
+    const user = await this.userRepository.create({
+      ...dto,
+      password: hashedPassword,
+    });
+
+    return {
+      id: user.id as number,
+      firstname: user.firstName,
+      username: user.username,
+      email: user.email,
+    };
   }
 }
