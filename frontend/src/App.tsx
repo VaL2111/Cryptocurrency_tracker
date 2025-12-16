fd77f4349ea4f336cf711679a78ccac2fb24fd05
Valentyn Bakhmatskyi <val.bakhmatskyi@gmail.com>
2025-12-16 20:09:07 +0200

    feat(frontend): initialize React application using Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./home/index.tsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home page</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
